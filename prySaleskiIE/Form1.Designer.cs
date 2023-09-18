ba9a56e3a6de19e6c1353f9fb6c72568be6c14c5
sacha saleski <saleski22.ss@gmail.com>
2023-09-18 14:08:59 -0300

    CAMBIOS

--- prySaleskiIE/Form1.Designer.cs
+++ prySaleskiIE/Form1.Designer.cs
@@ -29,20 +29,66 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Nombre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Detalle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(223, 408);
+            this.treeView1.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Nombre,
+            this.Tipo,
+            this.Detalle});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(229, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(409, 408);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Nombre
+            // 
+            this.Nombre.Text = "Nombre";
+            // 
+            // Tipo
+            // 
+            this.Tipo.Text = "Tipo";
+            // 
+            // Detalle
+            // 
+            this.Detalle.Text = "Detalle";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(918, 615);
+            this.ClientSize = new System.Drawing.Size(641, 410);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Nombre;
+        private System.Windows.Forms.ColumnHeader Tipo;
+        private System.Windows.Forms.ColumnHeader Detalle;
     }
 }
--- prySaleskiIE/Form1.cs
+++ prySaleskiIE/Form1.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+
 namespace prySaleskiIE
 {
     public partial class Form1 : Form
@@ -15,6 +17,47 @@ namespace prySaleskiIE
         public Form1()
         {
             InitializeComponent();
+            LlenarTreeView();
+        }
+        private void LlenarTreeView()
+        {
+            TreeNode nodoMadre;
+
+            DirectoryInfo info = new DirectoryInfo(@"E:/Documentos");
+            if (info.Exists == true) //POR DEFECTO el IF pregunta true
+            {
+                nodoMadre = new TreeNode(info.Name);
+                nodoMadre.Tag = info;
+                ObtenerCarpetas(info.GetDirectories(), nodoMadre);
+                treeView1.Nodes.Add(nodoMadre);
+            }
+        }
+        private void ObtenerCarpetas(DirectoryInfo[] subDirs,
+   TreeNode nodeToAddTo)
+        {
+            TreeNode aNode;
+            DirectoryInfo[] subSubDirs;
+
+            foreach (DirectoryInfo subDir in subDirs)
+            {
+                aNode = new TreeNode(subDir.Name, 0, 0);
+                aNode.Tag = subDir;
+                aNode.ImageKey = "folder";
+
+                //recursiva - se llama a si mismo para
+                //buscar màs carpetas
+                subSubDirs = subDir.GetDirectories();
+                if (subSubDirs.Length != 0)
+                {
+                    ObtenerCarpetas(subSubDirs, aNode);
+                }
+
+                nodeToAddTo.Nodes.Add(aNode);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
         }
     }
 }
